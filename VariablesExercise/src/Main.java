aea9df375dbf8a28c2edcf8a217ba5216a567b75
Georgina <george.parkerrr@gmail.com>
2024-10-23 15:11:02 +0100

    EDIT: main.java with task 2

--- VariablesExercise/src/Main.java
+++ VariablesExercise/src/Main.java
@@ -1,9 +1,20 @@
 
 public class Main {
     public static void main(String[] args) {
-        String firstName = "George", lastName = "Parker";
-        String name = firstName + " " + lastName;
-        System.out.println(name);
+//        String firstName = "George", lastName = "Parker";
+//        String name = firstName + " " + lastName;
+//        System.out.println(name);
+
+        int num1, num2;
+        num1 = 10;
+        num2 = 15;
+        System.out.println(num1+num2);
+        double result = (double)num1 / (double)num2;
+        System.out.println(result);
+        int num3 = 8;
+        System.out.println(num3++); // prints 8, then increments 8 to 9
+        System.out.println(num3); // prints 9
+        System.out.println(++num3); // increments 9 to 10, then prints 10
 
     }
 }
